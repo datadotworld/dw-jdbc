851c717cbcf7eba63baa748f42c9c50fce83623f
Dave Griffith <dave.griffith@data.world>
2016-11-10 15:35:02 -0600

    licensing

--- src/main/java/world/data/jdbc/results/DataWorldResultsSet.java
+++ src/main/java/world/data/jdbc/results/DataWorldResultsSet.java
@@ -1,4 +1,27 @@
+/*
+* dw-jdbc
+* Copyright 2016 data.world, Inc.
+
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the
+* License.
+*
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+* implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*
+* This product includes software developed at data.world, Inc.(http://www.data.world/).
+*/
 package world.data.jdbc.results;
+
+import org.apache.jena.graph.Node;
+import org.apache.jena.jdbc.JdbcCompatibility;
+import org.apache.jena.jdbc.utils.JdbcNodeUtils;
+import world.data.jdbc.statements.DataWorldStatement;
 
 import java.io.InputStream;
 import java.io.Reader;
@@ -23,12 +46,6 @@ import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Calendar;
 import java.util.Map;
-
-import org.apache.jena.graph.Node ;
-import org.apache.jena.jdbc.JdbcCompatibility;
-import org.apache.jena.jdbc.results.metadata.columns.DoubleColumn;
-import org.apache.jena.jdbc.utils.JdbcNodeUtils;
-import world.data.jdbc.statements.DataWorldStatement;
 
 /**
  * Abstract implementation of a JDBC Result Set which makes all update methods
